7e3772695cbac1bb7dd64b139505c5d422cb1a9a
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-03 13:41:59 -0400

    Initial commit

---
+++ src/com/cristianml/gui/form.java
@@ -0,0 +1,10 @@
+package com.cristianml.gui;
+
+import javax.swing.*;
+
+public class form {
+    private JButton RESETButton;
+    private JButton STARTButton;
+    private JLabel lblTitle;
+    private JLabel lblTime;
+}
